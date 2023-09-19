c4c2581d7fe171f8153512bf5e7dc75c59bf7dcb
Muneeb <muneebafzal381a@gmail.com>
2023-09-19 12:23:45 +0500

    changes in home page

---
+++ app/(root)/(home)/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Page = () => {
+  return (
+    <main className='flex-center paddings mx-auto w-full max-w-screen-2xl flex-col'>
+      <section className='nav-padding w-full'>
+        <div>
+          <h1 className='sm:heading1 heading2'>Javascript Mastery Resources</h1>
+        </div>
+      </section>
+    </main>
+  )
+}
+
+export default Page
